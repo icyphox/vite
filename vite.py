b2934e85c2f0e62ca9e78b7685acd128b73ba4d1
icyphox <icyph0x@protonmail.com>
2018-03-16 00:33:38 +0530

    feat: create project dirs

--- vite.py
+++ vite.py
@@ -1,6 +1,12 @@
+"""
+Vite - A simple and minimal static site generator.
+"""
+
 import markdown2
 import sys
 import argparse
+import os
+import errno
 
 parser = argparse.ArgumentParser(description='A simple and mnml static site generator.')
 parser.add_argument('action', choices=['new'], help='Create a new project.')
@@ -13,3 +19,8 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 project_path = args.path[0]
 
+try:
+    os.makedirs(project_path)
+except OSError as e:
+    if e.errno != errno.EEXIST
+        raise

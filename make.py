dda10020a83e9cc3030ace6a0aa99279fd44586b
icyphox <icyph0x@protonmail.com>
2018-03-16 11:33:27 +0530

    refactor(make.py): better filename

--- make.py
+++ make.py
@@ -8,6 +8,8 @@ BUILD_PATH = 'build/'
 
 for filename in os.listdir(PAGES_PATH):
     html = markdown_path(PAGES_PATH + filename)
-    with open(BUILD_PATH + filename + '.html', 'w') as f:
+    html_file = os.path.splitext(filename)[0] + '.html'
+    with open(BUILD_PATH + html_file, 'w') as f:
         f.write(html)
+        print('Rendered %s.' % (html_file))
         

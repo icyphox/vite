d1ab43a42712ddc6982432c82d40933a7906a2f2
icyphox <icyph0x@protonmail.com>
2018-03-16 10:15:42 +0530

    fix(make.py): path issues

--- make.py
+++ make.py
@@ -1,11 +1,12 @@
 from markdown2 import markdown_path
 from glob import glob
+import os
 
 # constants
 PAGES_PATH = 'pages/'
 BUILD_PATH = 'build/'
 
-for filename in glob(PAGES_PATH + '*.md'):
+for filename in os.listdir(PAGES_PATH):
     html = markdown_path(PAGES_PATH + filename)
     with open(BUILD_PATH + filename + '.html', 'w') as f:
         f.write(html)

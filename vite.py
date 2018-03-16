aa308328654b0db112bb7a2c261cd9f95c73f5b4
icyphox <icyph0x@protonmail.com>
2018-03-16 09:31:19 +0530

    feat: display path error

--- vite.py
+++ vite.py
@@ -21,11 +21,12 @@ project_path = args.path[0]
 
 def create_dirs(path):
     try:
+        abs_path = pathlib.Path(path).resolve()
         pathlib.Path(path + '/pages').mkdir(parents=True, exist_ok=False)
         pathlib.Path(path + '/build').mkdir(exist_ok=False)
+        print('Created project directory at %s.' % (abs_path))
     except FileExistsError as e:
-        if e.errno != errno.EEXIST:
-            raise
+        print('Error: specified path exists.')
 
 def main():
     create_dirs(project_path)

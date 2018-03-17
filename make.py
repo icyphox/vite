95661b45bb318ed2976ee68a63bc9aaa74e60ffc
icyphox <icyph0x@protonmail.com>
2018-03-17 13:43:40 +0530

    feat(make.py): now supports templating!

--- make.py
+++ make.py
@@ -6,14 +6,14 @@ import jinja2
 # constants
 PAGES_PATH = 'pages/'
 BUILD_PATH = 'build/'
-TEMPL_PATH = 'templates/'
+TEMPL_PATH = 'templates'
 
 # jinja2
 def jinja_render(html_text, template_file):
-    template_loader = jinja2.FileSystemLoader(searchpath=TEMPL_PATH)
+    template_loader = jinja2.FileSystemLoader('./')
     env = jinja2.Environment(loader=template_loader)
     template = env.get_template(template_file)
-    output = template.render(body=html_text)
+    output = template.render(title='test', body=html_text)
     return output
 
 def markdown_render(filename):
@@ -21,10 +21,14 @@ def markdown_render(filename):
     return html_text
 
 def main():
-    template_file = TEMPL_PATH + 'template.html'
+    template_file = TEMPL_PATH + '/template.html'
     for page in os.listdir(PAGES_PATH):
         html_text = markdown_render(page)
+        html_file = os.path.splitext(page)[0] + '.html'
         output = jinja_render(html_text, template_file)
-        with open(BUILD_PATH + html_text, 'w') as f:
+        with open(BUILD_PATH + html_file, 'w') as f:
             f.write(output)
             print('Rendered %s' % (page))
+
+if __name__ == "__main__":
+    main()

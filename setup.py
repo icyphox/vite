84deab9e9bb7b8d154c37a1bf867e5dab5abc53e
Anirudh <icyph0x@pm.me>
2018-04-26 21:15:00 +0530

    docs(setup.py): add author info

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
+        author='Anirudh',
+        author_email='icyph0x@pm.me',
         long_description=long_description,
         long_description_content_type='text/markdown',
         name='vite',
         version='1.2',
         description='A simple and minimal static site generator.',
+        url='https://github.com/icyphox/vite',
         packages=['vite'],
         install_requires=[
             'markdown2', 'Jinja2', 'huepy',

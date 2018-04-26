8c8b96f8c6456465d7eca4e383ab0006a94f4aa2
Anirudh <icyph0x@pm.me>
2018-04-26 17:11:02 +0530

    chore: version bump in `setup.py`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='vite',
-        version='1.0.0',
+        version='1.1',
         description='A simple and minimal static site generator.',
         packages=['vite'],
         install_requires=[
